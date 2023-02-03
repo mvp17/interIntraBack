f721437ffe59e7c6d0b68f004ea4101f89aae3af
mvp17 <mvisapascual@gmail.com>
2023-02-03 14:54:48 +0100

    Project init.

---
+++ src/test/kotlin/com/interapp/externa/InternetApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.interapp.externa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InternetApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
