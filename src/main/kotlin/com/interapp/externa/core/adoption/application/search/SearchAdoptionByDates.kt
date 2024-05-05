0990166108886e3cccbeb49d7b303b05c169bdb5
mvp17 <mvisapascual@gmail.com>
2024-05-05 20:11:25 +0200

    Fix search godfathers and adoptions by dates.

--- src/main/kotlin/com/interapp/externa/core/adoption/application/search/SearchAdoptionByDates.kt
+++ src/main/kotlin/com/interapp/externa/core/adoption/application/search/SearchAdoptionByDates.kt
@@ -3,16 +3,30 @@ package com.interapp.externa.core.adoption.application.search
 import com.interapp.externa.core.adoption.domain.Adoption
 import com.interapp.externa.core.adoption.domain.repositories.SearchByAdoptionRepo
 import org.springframework.stereotype.Service
+import java.text.SimpleDateFormat
 
 @Service
 class SearchAdoptionByDates(private val searchByAdoptionRepo: SearchByAdoptionRepo) {
+    private val inputFormat = SimpleDateFormat("dd-MM-yyyy")
+    private val outputFormat = SimpleDateFormat("yyyy-MM-dd")
     fun findAdoptionsByAdoptionFromDate(date: String): MutableList<Adoption> {
-        return searchByAdoptionRepo.findAdoptionsByFromDate(date)
+        val parsedDate = inputFormat.parse(date)
+        val outputDateString = outputFormat.format(parsedDate)
+
+        return searchByAdoptionRepo.findAdoptionsByFromDate(outputDateString)
     }
     fun findAdoptionsByAdoptionToDate(date: String): MutableList<Adoption> {
-        return searchByAdoptionRepo.findAdoptionsByToDate(date)
+        val parsedDate = inputFormat.parse(date)
+        val outputDateString = outputFormat.format(parsedDate)
+
+        return searchByAdoptionRepo.findAdoptionsByToDate(outputDateString)
     }
     fun findAdoptionsByAdoptionFromDateAndAdoptionToDate(fromDate: String, toDate: String): MutableList<Adoption> {
-        return searchByAdoptionRepo.findAdoptionsByAdoptionFromDateAndAdoptionToDate(fromDate, toDate)
+        val parsedFromDate = inputFormat.parse(fromDate)
+        val outputFromDateString = outputFormat.format(parsedFromDate)
+        val parsedToDate = inputFormat.parse(toDate)
+        val outputToDateString = outputFormat.format(parsedToDate)
+
+        return searchByAdoptionRepo.findAdoptionsByAdoptionFromDateAndAdoptionToDate(outputFromDateString, outputToDateString)
     }
 }
--- src/main/kotlin/com/interapp/externa/core/godfather/application/search/SearchGodfatherByBirthdayDates.kt
+++ src/main/kotlin/com/interapp/externa/core/godfather/application/search/SearchGodfatherByBirthdayDates.kt
@@ -3,18 +3,32 @@ package com.interapp.externa.core.godfather.application.search
 import com.interapp.externa.core.godfather.domain.Godfather
 import com.interapp.externa.core.godfather.domain.repositories.GodfatherRepo
 import org.springframework.stereotype.Service
+import java.text.SimpleDateFormat
 
 @Service
 class SearchGodfatherByBirthdayDates(private val godfatherRepo: GodfatherRepo) {
+    private val inputFormat = SimpleDateFormat("dd-MM-yyyy")
+    private val outputFormat = SimpleDateFormat("yyyy-MM-dd")
     fun findGodfathersByBirthdayFromDate(date: String): MutableList<Godfather> {
-        return godfatherRepo.findGodfathersByBirthdayFromDate(date)
+        val parsedDate = inputFormat.parse(date)
+        val outputDateString = outputFormat.format(parsedDate)
+
+        return godfatherRepo.findGodfathersByBirthdayFromDate(outputDateString)
     }
 
     fun findGodfathersByBirthdayToDate(date: String): MutableList<Godfather> {
-        return godfatherRepo.findGodfathersByBirthdayToDate(date)
+        val parsedDate = inputFormat.parse(date)
+        val outputDateString = outputFormat.format(parsedDate)
+
+        return godfatherRepo.findGodfathersByBirthdayToDate(outputDateString)
     }
 
     fun findGodfathersByBirthdayFromDateAndBirthdayToDate(fromDate: String, toDate: String): MutableList<Godfather> {
-        return godfatherRepo.findGodfathersByBirthdayFromDateAndBirthdayToDate(fromDate, toDate)
+        val parsedFromDate = inputFormat.parse(fromDate)
+        val outputFromDateString = outputFormat.format(parsedFromDate)
+        val parsedToDate = inputFormat.parse(toDate)
+        val outputToDateString = outputFormat.format(parsedToDate)
+
+        return godfatherRepo.findGodfathersByBirthdayFromDateAndBirthdayToDate(outputFromDateString, outputToDateString)
     }
 }
